0fd3e5fc580a7102bca01e7df35f8daa9fcfe22b
smurf <jelezarov.vladimir@gmail.com>
2017-08-07 12:59:29 +0200

    solution up to 1_20

--- Chapter_1/1_20/1_20.c
+++ Chapter_1/1_20/1_20.c
@@ -2,6 +2,18 @@
 
 int main(void)
 {
-	
+	int c;
+
+	while((c = getchar()) != EOF)
+	{
+		if (c == '\t')
+		{
+			//printf("    ");
+			printf("____");	// better visibility
+		} else
+		{
+			putchar(c);
+		}
+	}
 	return 0;
 }
