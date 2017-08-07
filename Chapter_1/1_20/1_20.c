f33cf1ddefd3d4874ae9a9741634526654267f17
smurf <jelezarov.vladimir@gmail.com>
2017-08-07 13:28:59 +0200

    bugfix

--- Chapter_1/1_20/1_20.c
+++ Chapter_1/1_20/1_20.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
+//#define n 8	// works too but we prefer variable
 
 int main(void)
 {
 	int c;
+	int col = 0;
+	int n = 8;
 
 	while((c = getchar()) != EOF)
 	{
-		if (c == '\t')
+		if (c == '\n')
 		{
-			//printf("    ");
-			printf("____");	// better visibility
+			putchar(c);
+			col = 0;
+		} else if (c == '\t')
+		{
+			for (int i = 0; i < (n - col); i++)
+			{
+				putchar(' ');
+				//putchar('_');	// better visibility
+			}
+			col = 0;
 		} else
 		{
 			putchar(c);
+			col++;
+			if (col == n)
+			{
+				col = 0;
+			}
 		}
 	}
 	return 0;
