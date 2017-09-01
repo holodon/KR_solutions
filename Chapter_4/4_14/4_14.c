60fa16d5dfa789cc6d0e39e88a2308df6f085340
smurf <jelezarov.vladimir@gmail.com>
2017-09-01 13:54:13 +0200

    solutions up to 4_14

--- Chapter_4/4_14/4_14.c
+++ Chapter_4/4_14/4_14.c
@@ -5,8 +5,18 @@ type t. (Block structure will help.)
 
 #include <stdio.h>
 
+#define swap(t,x,y) do {t = x; x = y; y = t;} while (0);
 
 int main(void)
 {
+    int t, x, y;
+
+    x = 2;
+    y = 3;
+
+    printf("x: %d, y: %d\n", x, y);
+    swap(t,x,y);
+    printf("x: %d, y: %d\n", x, y);
+
     return 0;
 }
