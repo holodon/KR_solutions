fe6b90a984e5214a5b17d872f2652d7e168cb007
Vladimir <jelezarov.vladimir@gmail.com>
2017-12-15 18:21:36 +0100

    WIP

---
+++ Chapter_6/6_6/test.c
@@ -0,0 +1,18 @@
+/*
+This is a test C source file to test 6_6.
+*/
+
+#include <stdio.h>
+
+#define GREATINGS "hello there"
+#define MCONST 333
+#undef MCONST
+
+int main(void)
+{
+    printf("%s %i\n", GREATINGS, MCONST);
+}
+
+#define SOME_NEW1    123
+
+#define WRONG "so wrong
